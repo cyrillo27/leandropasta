2debbec208fa6dfb563302a4430cb43bb503e77c
LEANDRO CYRILLO RAMOS <leandro.cramos2@senacsp.edu.br>
2024-07-30 20:33:36 -0300

    30.07

---
+++ 10/banco.php
@@ -0,0 +1,29 @@
+<?php
+
+//função para conectar ao banco de dados
+function connect() {
+
+}
+
+// função para obter todos os itens (read)
+function getAllItems() {
+
+}
+
+// Função para adicionar um novo item
+function addItem($nome_produto, $quantidade) {
+    
+}
+
+// funçao para atualizar um item
+function updateitem($id, $comprado) {
+
+}
+// Função para excluir um item (Delete)
+function deleteItem($id) {
+
+} 
+
+
+
+?>
