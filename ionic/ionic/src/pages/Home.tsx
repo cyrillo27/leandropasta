5757084923ed08205cbef41765279bb8e1f99a43
LEANDRO CYRILLO RAMOS <leandro.cramos2@senacsp.edu.br>
2024-11-25 19:37:22 -0300

    25

--- ionic/ionic/src/pages/Home.tsx
+++ ionic/ionic/src/pages/Home.tsx
@@ -20,13 +20,13 @@ const Home = () => {
           </IonToolbar>
         </IonHeader>
 
-        {/* <div id="container">
+        <div id="container">
           <strong>Ready to create an app?</strong>
           <p>Start with Ionic 
             <a target="_blank" rel="noopener noreferrer" 
             href="https://ionicframework.com/docs/components">UI Components</a>
           </p>
-        </div> */}
+        </div>
 
       </IonContent>
 
--- ionic/ionic/src/pages/Onboard.tsx
+++ ionic/ionic/src/pages/Onboard.tsx
@@ -1,33 +1,47 @@
-import { IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { bookOutline } from 'ionicons/icons';
-import "./Onboard.css";
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFooter, IonIcon } from '@ionic/react';
+import { homeOutline, libraryOutline } from 'ionicons/icons';
+import './Home.css';
 
-function Onboard () {
+const Home = () => {
+  return (
+    <IonPage>
 
-    return (
-        <IonContent fullscreen>
-            <div className="onboard-page">
-                <div className='header'>
-                    <IonIcon icon={bookOutline}></IonIcon>
-                </div>
-                <div className='body-content'>
-                    <div className='number'>
-                    
-                    </div>
-                    <div className="desc">
-                       
-                    </div>
-                </div>
-                <div className='control'>
-                    <ul className='dots'>
-                        <li className='active'></li>
-                        <li></li>
-                    </ul>
-                    <div className="arrow"></div>
-                </div>
-            </div>
-        </IonContent>
-    )
-}
+      {}
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle className="header-title">O Melhor do Pagode</IonTitle>
+        </IonToolbar>
+      </IonHeader>
 
-export default Onboard;
+      {}
+      <IonContent fullscreen>
+        <div className="dropdown-container">
+          <label htmlFor="pagode-options" className="dropdown-label">CDs de Pagode</label>
+          <select id="pagode-options" className="dropdown">
+            <option value="pixote">Pixote 15 Anos</option>
+            <option value="exaltasamba">Exaltasamba ao Vivo</option>
+            <option value="revelacao">Revelação 2003</option>
+            <option value="sensacao">Sensação ao Vivo</option>
+          </select>
+        </div>
+      </IonContent>
+
+      {}
+      <IonFooter>
+        <div className="footer">
+          <div className="footer-item">
+            <IonIcon icon={homeOutline} />
+            <p>início</p>
+          </div>
+          <div className="footer-item">
+            <IonIcon icon={libraryOutline} />
+            <p>biblioteca</p>
+          </div>
+        </div>
+      </IonFooter>
+
+    </IonPage>
+  );
+};
+
+export default Home;
